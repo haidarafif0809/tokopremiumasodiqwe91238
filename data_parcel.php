dac523bc96b51392ed40604af5fdb93456857e32
RindangRamadhan <rindangramadhan10@gmail.com>
2016-12-30 17:13:04 +0700

    PERAKITAN PARCEL-- RindangRamadhan
    Membuat Master Data untuk Perakitan Parcel

---
+++ data_parcel.php
@@ -0,0 +1,480 @@
+<?php include 'session_login.php';
+
+//memasukkan file session login, header, navbar, db.php
+include 'header.php';
+include 'navbar.php';
+include 'sanitasi.php';
+include 'db.php';
+
+?>
+
+<!-- js untuk tombol shortcut -->
+ <script src="shortcut.js"></script>
+<!-- js untuk tombol shortcut -->
+
+<style>
+	tr:nth-child(even){background-color: #f2f2f2}
+</style>
+
+
+ <div class="container">
+
+ <h3><b> DATA PARCEL</b></h3><hr>
+<div class="row">
+<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-shopping-basket" id="tambah_parcel"> </i>
+PARCEL (F1) </button>
+
+
+<!--MODAL EDIT -->
+<div id="modal_edit" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">EDIT DATA PARCEL</h4>
+      </div>
+      <div class="modal-body">
+
+<form class="form" role="form" id="formparceledit">
+		
+		<div class="row">
+		<div class="col-sm-6">	
+
+		  
+		  	<label>Nama Parcel</label>
+		    <input style="height:15px;" type="text" class="form-control" name="nama_parcel" autocomplete="off" id="nama_parcel_edit" placeholder="NAMA PARCEL">
+
+		  
+		  	<label>Harga 1</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_1" autocomplete="off" id="harga_parcel_edit_1" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 1">
+
+		  
+		  	<label>Harga 2</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_2" autocomplete="off" id="harga_parcel_edit_2" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 2">
+
+		  
+		  	<label>Harga 3</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_3" autocomplete="off" id="harga_parcel_edit_3" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 3">
+
+		</div>
+		
+
+		<div class="col-sm-6">
+
+		  
+		  	<label>Harga 4</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_4" autocomplete="off" id="harga_parcel_edit_4" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 4">
+
+		  
+		  	<label>Harga 5</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_5" autocomplete="off" id="harga_parcel_edit_5" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 5">
+
+		  
+		  	<label>Harga 6</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_6" autocomplete="off" id="harga_parcel_edit_6" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 6">
+
+		  
+		  	<label>Harga 7</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_7" autocomplete="off" id="harga_parcel_edit_7" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 7">
+
+
+		  <input style="height:15px;" type="hidden" class="form-control" name="id_edit" autocomplete="off" id="id_edit" placeholder="HARGA 7">
+
+		</div>
+		</div>
+
+		  	  <button type="submit" id="simpan_parcel" class="btn btn-success" style="font-size:15px"> <i class="fa fa-plus"> </i> Simpan (F3)</button>
+		 
+		
+		
+	</form> <!-- END FORM -->
+
+  <div class="alert alert-success" style="display:none">
+   <strong>Berhasil!</strong> Data Berhasil Di Edit
+  </div>
+ 
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" id="close_edit" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- END MODAL EDIT  -->
+
+
+<div class="collapse" id="collapseExample">
+
+	<form class="form" role="form" id="formparcel">
+		<div class=" card card-block">
+		<div class="row">
+		  <div class="col-sm-2">
+		  	<label>Kode Parcel</label>
+		    <input type="text" style="height:15px" class="form-control" name="kode_parcel" autocomplete="off" id="kode_parcel" placeholder="KODE PARCEL">
+		  </div>
+
+		  <div class="col-sm-2">
+		  	<label>Nama Parcel</label>
+		    <input style="height:15px;" type="text" class="form-control" name="nama_parcel" autocomplete="off" id="nama_parcel" placeholder="NAMA PARCEL">
+		  </div>
+
+		  <div class="col-sm-2">
+		  	<label>Harga 1</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_1" autocomplete="off" id="harga_parcel_1" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 1">
+		  </div>
+
+		  <div class="col-sm-2">
+		  	<label>Harga 2</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_2" autocomplete="off" id="harga_parcel_2" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 2">
+		  </div>
+
+		  <div class="col-sm-2">
+		  	<label>Harga 3</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_3" autocomplete="off" id="harga_parcel_3" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 3">
+		  </div>
+
+		</div>
+
+		<div class="row">
+
+		  <div class="col-sm-2">
+		  	<label>Harga 4</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_4" autocomplete="off" id="harga_parcel_4" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 4">
+		  </div>
+
+		  <div class="col-sm-2">
+		  	<label>Harga 5</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_5" autocomplete="off" id="harga_parcel_5" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 5">
+		  </div>
+
+		  <div class="col-sm-2">
+		  	<label>Harga 6</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_6" autocomplete="off" id="harga_parcel_6" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 6">
+		  </div>
+
+		  <div class="col-sm-2">
+		  	<label>Harga 7</label>
+		    <input style="height:15px;" type="text" class="form-control" name="harga_parcel_7" autocomplete="off" id="harga_parcel_7" onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);" placeholder="HARGA 7">
+		  </div>
+
+
+		  <div class="col-sm-2">
+		  <label><br><br><br></label>
+		  	  <button type="submit" id="submit_parcel" class="btn btn-info" style="font-size:15px"> <i class="fa fa-plus"> </i> Tambah (F2)</button>
+		  </div>
+		</div>
+		</div> <!-- END CARD BLOCK -->
+	</form> <!-- END FORM -->
+
+</div> <!-- END collapseExample -->
+
+
+<div class="table-responsive"><!--membuat agar ada garis pada tabel disetiap kolom-->
+<span id="tabel-parcel">
+	<table id="tabel_parcel" class="display table table-bordered table-sm">
+		<thead>
+		
+
+			<th style='background-color: #4CAF50; color: white'> Kode Parcel </th>
+			<th style='background-color: #4CAF50; color: white'> Nama Parcel</th>
+			<th style='background-color: #4CAF50; color: white'> Harga Parcel 1</th>
+			<th style='background-color: #4CAF50; color: white'> Harga Parcel 2</th>
+			<th style='background-color: #4CAF50; color: white'> Harga Parcel 3</th>
+			<th style='background-color: #4CAF50; color: white'> Harga Parcel 4</th>
+			<th style='background-color: #4CAF50; color: white'> Harga Parcel 5</th>
+			<th style='background-color: #4CAF50; color: white'> Harga Parcel 6</th>
+			<th style='background-color: #4CAF50; color: white'> Harga Parcel 7</th>
+			<th style='background-color: #4CAF50; color: white'> Petugas Input</th>
+			<th style='background-color: #4CAF50; color: white'> Petugas Edit</th>
+			<th style='background-color: #4CAF50; color: white'> Edit </th>
+			<th style='background-color: #4CAF50; color: white'> Hapus </th>
+
+		
+		</thead>
+	</table>
+</span>
+</div>
+
+</div> <!-- END CONTAINER -->
+
+
+<script type="text/javascript">
+ $("#kode_parcel").blur(function(){
+    var kode_parcel = $("#kode_parcel").val();
+	 $.post("cek_data_parcel.php",{kode_parcel:kode_parcel},function(data){    
+	     
+	     if (data > 0) {
+	          alert("KODE PARCEL SUDAH ADA !!");
+	          $("#kode_parcel").val('');
+	          $("#kode_parcel").focus();
+	     }
+	     
+	 });
+});
+</script>
+
+
+<!--perintah javascript yang diambil dari form proses_bayar_beli.php dengan id=form_beli -->
+<script>
+  $(document).on('click','#submit_parcel',function(){
+
+    var kode_parcel = $("#kode_parcel").val();
+    var nama_parcel = $("#nama_parcel").val();
+    var harga_parcel_1 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_1").val()))));
+    var harga_parcel_2 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_2").val()))));
+    var harga_parcel_3 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_3").val()))));
+    var harga_parcel_4 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_4").val()))));
+    var harga_parcel_5 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_5").val()))));
+    var harga_parcel_6 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_6").val()))));
+    var harga_parcel_7 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_7").val()))));
+       
+
+ if (kode_parcel == "") {
+  	alert ("SILAKAN ISI KODE PARCEL");
+  	$("#kode_parcel").focus();
+ }
+
+ else if (nama_parcel == "") {
+  	alert ("SILAKAN ISI NAMA PARCEL");
+  	$("#nama_parcel").focus();
+ }
+
+ else if (harga_parcel_1 == "") {
+  	alert ("SILAKAN ISI HARGA PARCEL 1");
+  	$("#harga_parcel_1").focus();
+ }
+
+ else
+ {
+
+
+	$("#tambah_parcel").click();	
+
+ 	$.post("proses_input_parcel.php",{kode_parcel:kode_parcel, nama_parcel:nama_parcel, harga_parcel_1:harga_parcel_1, harga_parcel_2:harga_parcel_2, harga_parcel_3:harga_parcel_3, harga_parcel_4:harga_parcel_4, harga_parcel_5:harga_parcel_5, harga_parcel_6:harga_parcel_6, harga_parcel_7:harga_parcel_7},function(data){
+     
+     $('#tabel_parcel').DataTable().destroy();
+     
+      var dataTable = $('#tabel_parcel').DataTable( {
+          "processing": true,
+          "serverSide": true,
+          "ajax":{
+            url :"datatable_perakitan_parcel.php", // json datasource
+            type: "post",  // method  , by default get
+            error: function(){  // error handling
+              $(".employee-grid-error").html("");
+              $("#tabel_parcel").append('<tbody class="employee-grid-error"><tr><th colspan="3">Data Tidak Ditemukan !!</th></tr></tbody>');
+              $("#employee-grid_processing").css("display","none");
+              }
+          },
+             "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+
+              $(nRow).attr('class','tr-id-'+aData[13]+'');         
+
+          }
+        });
+
+
+		$("#kode_parcel").val('');
+		$("#nama_parcel").val('');
+		$("#harga_parcel_1").val('');
+		$("#harga_parcel_2").val('');
+		$("#harga_parcel_3").val('');
+		$("#harga_parcel_4").val('');
+		$("#harga_parcel_5").val('');
+		$("#harga_parcel_6").val('');
+		$("#harga_parcel_7").val('');
+     
+     });
+
+ }//END ELSE
+
+
+	$("#formparcel").submit(function(){
+		return false;
+	});
+    
+
+}); //END FUNCITON CLICK
+      
+</script>
+
+
+<!--HAPUS PARCEL -->
+<script type="text/javascript">
+//jika dipilih, nim akan masuk ke input dan modal di tutup
+$(document).on('click', '.btn-hapus-parcel', function (e) {
+   
+   var id = $(this).attr('data-id');
+              
+    $(".tr-id-"+id+"").remove();
+    $.post("hapus_data_parcel.php",{id:id},function(data){
+
+   	});
+
+
+  var table = $('#tabel_parcel').DataTable(); 
+      table.row( $(this).parents('tr') ).remove().draw();
+
+});
+
+</script>
+<!--HAPUS PARCEL-->
+
+
+<!--EDIT PARCEL -->
+<script type="text/javascript">
+$(document).on('click', '.btn-edit-parcel', function (e) {
+	$("#modal_edit").modal('show');
+    var id_edit = $(this).attr("data-id");
+    var nama_parcel_edit = $(this).attr("data-nama");
+    var harga_parcel_edit_1 = $(this).attr("data-harga-1");
+    var harga_parcel_edit_2 = $(this).attr("data-harga-2");
+    var harga_parcel_edit_3 = $(this).attr("data-harga-3");
+    var harga_parcel_edit_4 = $(this).attr("data-harga-4");
+    var harga_parcel_edit_5 = $(this).attr("data-harga-5");
+    var harga_parcel_edit_6 = $(this).attr("data-harga-6");
+    var harga_parcel_edit_7 = $(this).attr("data-harga-7");
+
+		$("#id_edit").val(id_edit);
+		$("#nama_parcel_edit").val(nama_parcel_edit);
+		$("#harga_parcel_edit_1").val(tandaPemisahTitik(harga_parcel_edit_1));
+		$("#harga_parcel_edit_2").val(tandaPemisahTitik(harga_parcel_edit_2));
+		$("#harga_parcel_edit_3").val(tandaPemisahTitik(harga_parcel_edit_3));
+		$("#harga_parcel_edit_4").val(tandaPemisahTitik(harga_parcel_edit_4));
+		$("#harga_parcel_edit_5").val(tandaPemisahTitik(harga_parcel_edit_5));
+		$("#harga_parcel_edit_6").val(tandaPemisahTitik(harga_parcel_edit_6));
+		$("#harga_parcel_edit_7").val(tandaPemisahTitik(harga_parcel_edit_7));
+		
+		
+		});
+
+$(document).on('click','#simpan_parcel',function(e){
+	var id_edit = $("#id_edit").val();
+	var nama_parcel_edit = $("#nama_parcel_edit").val();
+    var harga_parcel_edit_1 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_edit_1").val()))));
+    var harga_parcel_edit_2 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_edit_2").val()))));
+    var harga_parcel_edit_3 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_edit_3").val()))));
+    var harga_parcel_edit_4 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_edit_4").val()))));
+    var harga_parcel_edit_5 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_edit_5").val()))));
+    var harga_parcel_edit_6 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_edit_6").val()))));
+    var harga_parcel_edit_7 = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#harga_parcel_edit_7").val()))));
+
+ if (nama_parcel_edit == "") {
+  	alert ("SILAKAN ISI NAMA PARCEL");
+  	$("#nama_parcel_edit").focus();
+ }
+
+ else if (harga_parcel_edit_1 == "") {
+  	alert ("SILAKAN ISI HARGA PARCEL 1");
+  	$("#harga_parcel_edit_1").focus();
+ }
+
+ else{
+ 
+
+	$("#close_edit").click();
+
+	 $.post("update_data_parcel.php",{id_edit:id_edit, nama_parcel_edit:nama_parcel_edit, harga_parcel_edit_1:harga_parcel_edit_1, harga_parcel_edit_2:harga_parcel_edit_2, harga_parcel_edit_3:harga_parcel_edit_3, harga_parcel_edit_4:harga_parcel_edit_4, harga_parcel_edit_5:harga_parcel_edit_5, harga_parcel_edit_6:harga_parcel_edit_6, harga_parcel_edit_7:harga_parcel_edit_7},function(data){
+     
+     $('#tabel_parcel').DataTable().destroy();
+     
+      var dataTable = $('#tabel_parcel').DataTable( {
+          "processing": true,
+          "serverSide": true,
+          "ajax":{
+            url :"datatable_perakitan_parcel.php", // json datasource
+            type: "post",  // method  , by default get
+            error: function(){  // error handling
+              $(".employee-grid-error").html("");
+              $("#tabel_parcel").append('<tbody class="employee-grid-error"><tr><th colspan="3">Data Tidak Ditemukan !!</th></tr></tbody>');
+              $("#employee-grid_processing").css("display","none");
+              }
+          },
+             "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+
+              $(nRow).attr('class','tr-id-'+aData[13]+'');         
+
+          }
+        });
+
+		$("#nama_parcel_edit").val('');
+		$("#harga_parcel_edit_1").val('');
+		$("#harga_parcel_edit_2").val('');
+		$("#harga_parcel_edit_3").val('');
+		$("#harga_parcel_edit_4").val('');
+		$("#harga_parcel_edit_5").val('');
+		$("#harga_parcel_edit_6").val('');
+		$("#harga_parcel_edit_7").val('');
+     
+     });
+
+}
+
+     $('#formparceledit').submit(function(){		
+		return false;
+	 });
+
+	
+});
+
+</script>
+<!--END EDIT PARCEL -->
+
+
+<!--- AJAX DATATABLE -->
+<script type="text/javascript">
+$(document).ready(function(){
+
+	  $('#tabel_parcel').DataTable().destroy();
+     
+      var dataTable = $('#tabel_parcel').DataTable( {
+          "processing": true,
+          "serverSide": true,
+          "ajax":{
+            url :"datatable_perakitan_parcel.php", // json datasource
+            type: "post",  // method  , by default get
+            error: function(){  // error handling
+              $(".employee-grid-error").html("");
+              $("#tabel_parcel").append('<tbody class="employee-grid-error"><tr><th colspan="3">Data Tidak Ditemukan !!</th></tr></tbody>');
+              $("#employee-grid_processing").css("display","none");
+              }
+          },
+             "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+
+              $(nRow).attr('class','tr-id-'+aData[13]+'');         
+
+          }
+        });
+
+
+});
+</script>
+<!--- AJAX DATATABLE -->
+
+
+
+
+
+<!-- SHORTCUT -->
+<script> 
+    shortcut.add("f1", function() {
+
+        $("#tambah_parcel").click();
+
+    });
+
+    
+    shortcut.add("f2", function() {
+
+        $("#submit_parcel").click();
+
+    });
+
+</script>
+<!-- SHORTCUT -->
+
+<?php include 'footer.php'; ?>
---
+++ datatable_perakitan_parcel.php
@@ -0,0 +1,91 @@
+<?php include 'session_login.php';
+/* Database connection start */
+include 'db.php';
+include 'sanitasi.php';
+
+// storing  request (ie, get/post) global array to a variable  
+$requestData= $_REQUEST;
+
+
+$columns = array( 
+// datatable column index  => database column name
+	0 =>'id', 
+	1 => 'kode_parcel',
+  2 => 'nama_parcel',
+  3 => 'harga_parcel',
+  4 => 'harga_parcel_2',
+  5 => 'harga_parcel_3',
+  6 => 'harga_parcel_4',
+  7 => 'harga_parcel_5',
+  8 => 'harga_parcel_6',
+  9 => 'harga_parcel_7',
+  10 => 'user_input',
+  11 => 'user_edit'
+
+);
+
+// getting total number records without any search
+
+
+$sql = "SELECT id, kode_parcel, nama_parcel, harga_parcel, harga_parcel_2, harga_parcel_3, harga_parcel_4, harga_parcel_5, harga_parcel_6, harga_parcel_7, user_input, user_edit ";
+$sql.=" FROM perakitan_parcel";
+$query=mysqli_query($conn, $sql) or die("Salahnya Disini 1");
+$totalData = mysqli_num_rows($query);
+$totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
+
+
+$sql = "SELECT id, kode_parcel, nama_parcel, harga_parcel, harga_parcel_2, harga_parcel_3, harga_parcel_4, harga_parcel_5, harga_parcel_6, harga_parcel_7, user_input, user_edit ";
+$sql.=" FROM perakitan_parcel WHERE 1=1";
+
+
+if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
+  $sql.=" AND ( kode_parcel LIKE '".$requestData['search']['value']."%' ";    
+  $sql.=" OR nama_parcel LIKE '".$requestData['search']['value']."%'";  
+  $sql.=" OR user_input LIKE '".$requestData['search']['value']."%'";    
+  $sql.=" OR user_edit LIKE '".$requestData['search']['value']."%' )";
+
+}
+$query=mysqli_query($conn, $sql) or die("Salahnya Disini 2");
+$totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
+
+$sql.=" ORDER BY ". $columns[$requestData['order'][0]['column']]."   ".$requestData['order'][0]['dir']."  LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
+/* $requestData['order'][0]['column'] contains colmun index, $requestData['order'][0]['dir'] contains order such as asc/desc  */
+
+$query=mysqli_query($conn, $sql) or die("Salahnya Disini 3");
+$data = array();
+
+while( $row=mysqli_fetch_array($query) ) {  // preparing an array
+	$nestedData=array();
+
+	$nestedData[] = $row["kode_parcel"];
+	$nestedData[] = $row["nama_parcel"];
+  $nestedData[] = rp($row["harga_parcel"]); 
+  $nestedData[] = rp($row["harga_parcel_2"]); 
+  $nestedData[] = rp($row["harga_parcel_3"]); 
+  $nestedData[] = rp($row["harga_parcel_4"]); 
+  $nestedData[] = rp($row["harga_parcel_5"]); 
+  $nestedData[] = rp($row["harga_parcel_6"]); 
+  $nestedData[] = rp($row["harga_parcel_7"]); 
+	$nestedData[] = $row["user_input"];
+  $nestedData[] = $row["user_edit"];
+
+  $nestedData[] = "<button class='btn btn-success btn-sm btn-edit-parcel' data-kode='". $row['kode_parcel'] ."' data-nama='". $row['nama_parcel'] ."' data-harga-1='". $row['harga_parcel'] ."' data-harga-2='". $row['harga_parcel_2'] ."' data-harga-3='". $row['harga_parcel_3'] ."' data-harga-4='". $row['harga_parcel_4'] ."' data-harga-5='". $row['harga_parcel_5'] ."' data-harga-6='". $row['harga_parcel_6'] ."' data-harga-7='". $row['harga_parcel_7'] ."' data-id='". $row['id'] ."' > Edit </button>";
+
+  $nestedData[] = "<button class='btn btn-danger btn-sm btn-hapus-parcel' data-id='". $row['id']."' > Hapus </button>";
+  $nestedData[] = $row["id"];
+	
+	$data[] = $nestedData;
+}
+
+
+
+$json_data = array(
+			"draw"            => intval( $requestData['draw'] ),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
+			"recordsTotal"    => intval( $totalData ),  // total number of records
+			"recordsFiltered" => intval( $totalFiltered ), // total number of records after searching, if there is no searching then totalFiltered = totalData
+			"data"            => $data   // total data array
+			);
+
+echo json_encode($json_data);  // send data as json format
+
+?>

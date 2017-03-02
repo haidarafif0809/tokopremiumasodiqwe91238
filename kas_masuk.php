fc8904487112f7e4bfa3569e0e726c93e556fd5d
hendriandaglos <hendri.syand@gmail.com>
2017-03-02 15:12:50 +0700

    ajax detail kas masuk

--- kas_masuk.php
+++ kas_masuk.php
@@ -115,7 +115,25 @@ tr:nth-child(even){background-color: #f2f2f2}
 
       <div class="modal-body">
       <div class="table-responsive">
+
       <span id="modal-detail"> </span>
+ 	
+ 	<table id="table_modal_detail" class="table table-bordered table-sm">
+  	<thead> <!-- untuk memberikan nama pada kolom tabel -->
+
+          <th> No Faktur </th>
+          <th> Dari Akun </th>
+          <th> Ke Akun </th>
+          <th> Jumlah Barang </th>
+          <th> Tanggal </th>
+          <th> Jam </th>
+          <th> Keterangan </th>
+          <th> Petugas </th>
+          
+  	</thead> <!-- tag penutup tabel -->
+  	</table>
+
+       <input type="hidden" name="no_faktur_detail" class="form-control " id="no_faktur_detail" placeholder="no_faktur  "/>
       </div>
 
      </div>
@@ -218,7 +236,49 @@ if ($kas_masuk['kas_masuk_hapus'] > 0) {
 		
 </script>
 
-<script type="text/javascript">
+
+
+<!--Start Ajax Modal DETAIL-->
+<script type="text/javascript" language="javascript" >
+   $(document).ready(function() {
+    $(document).on('click', '.detail', function (e) {
+    $("#modal_detail").modal('show');
+
+    var no_faktur = $(this).attr("no_faktur");
+    $("#no_faktur_detail").val(no_faktur);
+      var no_faktur_detail = $("#no_faktur_detail").val();
+          
+          $('#table_modal_detail').DataTable().destroy();
+
+        var dataTable = $('#table_modal_detail').DataTable( {
+          "processing": true,
+          "serverSide": true,
+          "ajax":{
+            url :"modal_detail_kas_masuk.php", // json datasource
+             "data": function ( d ) {
+                  d.no_faktur = $("#no_faktur_detail").val();
+                  // d.custom = $('#myInput').val();
+                  // etc
+              },
+            type: "post",  // method  , by default get
+            error: function(){  // error handling
+              $(".employee-grid-error").html("");
+              $("#table_modal_detail").append('<tbody class="employee-grid-error"><tr><th colspan="3">Data Tidak Ditemukan.. !!</th></tr></tbody>');
+              $("#employee-grid_processing").css("display","none");
+              
+            }
+          },
+
+         
+
+        });  
+  
+     }); 
+  });
+ </script>
+<!--Ending Ajax Modal Detail-->
+
+<!--<script type="text/javascript">
 		$(document).on('click','.detail',function(e){
 		var no_faktur = $(this).attr('no_faktur');
 		
@@ -234,12 +294,11 @@ if ($kas_masuk['kas_masuk_hapus'] > 0) {
 		
 		});
 		
-</script>
+</script>-->
 
 
 	
-<script type="text/javascript">
-			
+<script type="text/javascript">			
 //fungsi hapus data 
 		$(document).on('click','.btn-hapus',function(e){
 		var no_faktur = $(this).attr("no-faktur");

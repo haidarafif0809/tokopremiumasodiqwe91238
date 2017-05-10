5a50075fe836bda3a430ed3fe1123df3b82a6892
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-05-10 13:12:18 +0700

    perbaikan cek tbs stok opname

--- cek_kode_barang_tbs_stok_opname.php
+++ cek_kode_barang_tbs_stok_opname.php
@@ -2,11 +2,10 @@
 include 'db.php';
 include 'sanitasi.php';
 
-$kode_barang = stringdoang($_POST['kode_barang']);
 $session_id = session_id();
 
 
-$query = $db->query("SELECT kode_barang FROM tbs_stok_opname WHERE kode_barang = '$kode_barang' AND (no_faktur  = '' OR no_faktur IS NULL) ");
+$query = $db->query("SELECT kode_barang FROM tbs_stok_opname WHERE session_id = '$session_id' AND (no_faktur  = '' OR no_faktur IS NULL) ");
 $jumlah = mysqli_num_rows($query);
 
 

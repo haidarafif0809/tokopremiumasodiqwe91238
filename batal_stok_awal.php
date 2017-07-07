d9a58de779e33d47aaf85710a0acb7c5099ab155
Rikopernando <rikopernando96@gmail.com>
2017-07-07 10:54:03 +0700

    Perbaikan kartu stok dan batal stok awal
    Perbaikan kartu stok dan batal stok awal

--- batal_stok_awal.php
+++ batal_stok_awal.php
@@ -4,30 +4,21 @@ include 'db.php';
 
 //mengirimkan $id menggunakan metode GET
 
-$kode_barang = $_GET['kode_barang'];
+$query1 = $db->query("SELECT kode_barang FROM tbs_stok_awal ");
+while ($data =mysqli_fetch_array($query1)) {
+	
 
-
-
-$query1 = $db->query("SELECT * FROM tbs_stok_awal WHERE kode_barang = '$kode_barang'");
-$data =mysqli_fetch_array($query1);
-
-
-
+$query = $db->query("DELETE FROM tbs_stok_awal WHERE kode_barang = '$data[kode_barang]'");
     	
 $query = $db->query("UPDATE barang SET stok_awal = '' WHERE kode_barang = '$data[kode_barang]'");
 
-//menghapus seluruh data yang ada pada tabel tbs_pembelian berdasarkan id
-$query = $db->query("DELETE FROM tbs_stok_awal WHERE kode_barang = '$kode_barang'");
+}
+	
 
-//jika $query benar maka akan menuju file formpembelian.php , jika salah maka failed
-if ($query == TRUE)
-{
+
 	header('location:form_stok_awal.php');
-}
-else
-{
-	echo "gagal";
-}
+
+
 
         //Untuk Memutuskan Koneksi Ke Database
 

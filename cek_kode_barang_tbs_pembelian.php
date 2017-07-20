1df96af323961268d0e0e7eeebdbee0bce58207f
Rindang Ramadhan <rindangramadhan10@gmail.com>
2017-07-20 10:17:47 +0700

    Perbaikan Review Mas Ijal
    query select num_rows tidak perlu memakai bintang

--- cek_kode_barang_tbs_pembelian.php
+++ cek_kode_barang_tbs_pembelian.php
@@ -5,7 +5,7 @@ include 'db.php';
 $session_id = $_POST['session_id'];
 $kode_barang = $_POST['kode_barang'];
 
-$query = $db->query("SELECT * FROM tbs_pembelian WHERE kode_barang = '$kode_barang' AND session_id = '$session_id' AND no_faktur_order IS NULL");
+$query = $db->query("SELECT kode_barang, session_id FROM tbs_pembelian WHERE kode_barang = '$kode_barang' AND session_id = '$session_id' AND no_faktur_order IS NULL");
 $jumlah = mysqli_num_rows($query);
 
 

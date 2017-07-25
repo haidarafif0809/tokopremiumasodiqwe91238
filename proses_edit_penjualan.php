b73a6d36f61f1c4ab3a2fcb748808a151fd09b55
RindangRamadhan <rindangramadhan10@gmail.com>
2017-07-25 15:33:25 +0700

    proses_eddipenjualan

--- proses_edit_penjualan.php
+++ proses_edit_penjualan.php
@@ -22,7 +22,9 @@ $query_delete_tbs_bonus = $db->query("DELETE FROM tbs_bonus_penjualan WHERE no_f
 //menampilkan seluruh data yang ada pada tabel pembelian dalan DB
 $query_detail_penjualan = $db->query("SELECT no_faktur, kode_barang, nama_barang, jumlah_barang, satuan, asal_satuan, harga, subtotal, potongan, tax, hpp FROM detail_penjualan WHERE no_faktur = '$no_faktur'");
 while ($data_detail_penjualan = mysqli_fetch_array($query_detail_penjualan)){
-
+		
+		$query_bonus_penjualan = $db->query("SELECT no_faktur_penjualan,kode_pelanggan,kode_produk,nama_produk,satuan,harga_disc,qty_bonus,subtotal,keterangan FROM bonus_penjualan WHERE no_faktur_penjualan = '$no_faktur'");
+		$data_bonus_penjualan = mysqli_fetch_array($query_bonus_penjualan);
 
 
         $tipe = $db->query("SELECT berkaitan_dgn_stok FROM barang WHERE kode_barang = '$data[kode_barang]'");
@@ -45,15 +47,15 @@ while ($data_detail_penjualan = mysqli_fetch_array($query_detail_penjualan)){
         	$jumlah_barang = $data['jumlah_barang'];
         }
 
-		}// end else dari if ($data_detail_penjualan['satuan'] == $data_detail_penjualan['asal_satuan'])
 
 		$perintah1 = $db->query("INSERT INTO tbs_penjualan (no_faktur, kode_barang, nama_barang, jumlah_barang, satuan, harga, subtotal, potongan, tax, hpp,tipe_barang,harga_konversi) VALUES ( '$data[no_faktur]', '$data[kode_barang]', '$data[nama_barang]',
 			'$jumlah_barang', '$data[satuan]', '$data[harga]', '$data[subtotal]','$data[potongan]', '$data[tax]', '$data[hpp]','$ber_stok','$data[harga_konversi]')");
+		if($data_bonus_penjualan['no_faktur_penjualan'] == $no_faktur){
+				//insert_tbs_bonus  dari detail_bonus
+				$query_insert_tbs_bonus = $db->query("INSERT INTO tbs_bonus_penjualan (no_faktur_penjualan,kode_pelanggan,kode_produk,nama_produk,satuan,harga_disc,qty_bonus,subtotal,keterangan) VALUES ('$data_bonus_penjualan[no_faktur_penjualan]','$data_bonus_penjualan[kode_pelanggan]','$data_bonus_penjualan[kode_produk]','$data_bonus_penjualan[nama_produk]','$data_bonus_penjualan[satuan]','$data_bonus_penjualan[harga_disc]','$data_bonus_penjualan[qty_bonus]','$data_bonus_penjualan[subtotal]','$data_bonus_penjualan[keterangan]')");
+				}// end if ($data_bonus_penjualan['no_faktur_penjualan'] == $no_faktur)
 
-
-
-
-}
+}//end while
 
  header ('location:edit_penjualan.php?no_faktur='.$no_faktur.'&kode_pelanggan='.$kode_pelanggan.'&nama_gudang='.$nama_gudang.'&kode_gudang='.$kode_gudang.'');
 

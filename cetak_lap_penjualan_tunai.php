ae03477ec6bece5cde43fb0436cdb6209b7ec170
hendriandaglos <hendri.syand@gmail.com>
2017-07-28 10:00:35 +0700

    BUG & REQUEST
    Masalah cetak tunai, total item koma dua, di cetak form penjualan ataupun di laporan penjualan

--- cetak_lap_penjualan_tunai.php
+++ cetak_lap_penjualan_tunai.php
@@ -87,7 +87,7 @@ $no_faktur = $_GET['no_faktur'];
       <tr><td width="50%">Biaya Admin</td> <td> :</td> <td><?php echo koma($data0['biaya_admin'],2);?> </tr>
 
       <!--<tr><td  width="50%">Pajak</td> <td> :</td> <td> <?php echo koma($data0['tax']);?> </td></tr>-->
-      <tr><td  width="50%">Total Item</td> <td> :</td> <td> <?php echo koma($total_item,3); ?> </td></tr>
+      <tr><td  width="50%">Total Item</td> <td> :</td> <td> <?php echo koma($total_item,2); ?> </td></tr>
       <tr><td width="50%">Total Penjualan</td> <td> :</td> <td><?php echo koma($data0['total'],2); ?> </tr>
       <tr><td  width="50%">Tunai</td> <td> :</td> <td> <?php echo koma($data0['tunai'],2); ?> </td></tr>
       <tr><td  width="50%">Kembalian</td> <td> :</td> <td> <?php echo koma($data0['sisa'],2); ?>  </td></tr>

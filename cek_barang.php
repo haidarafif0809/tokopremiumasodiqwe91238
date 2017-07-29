a74e114f3aef1032234d4a7f84d9df66a2462b0e
hendriandaglos <hendri.syand@gmail.com>
2017-07-29 15:03:14 +0700

    BUG & REQUEST
    Input Produk lewat barcode di order penjualan ada data produk yang bisa di input dan tidak

--- cek_barang.php
+++ cek_barang.php
@@ -4,8 +4,7 @@ include 'db.php';
 
 $kode_barang = stringdoang($_GET['kode_barang']);
 
-$query = $db->query("SELECT COUNT(kode_barang) AS jumlah_data FROM barang INNER JOIN satuan_konversi ON barang.id = satuan_konversi.id_produk 
-	AND barang.kode_barang = satuan_konversi.kode_produk WHERE barang.kode_barang = '$kode_barang' OR satuan_konversi.kode_barcode = '$kode_barang' ");
+$query = $db->query("SELECT COUNT(kode_barang) AS jumlah_data FROM barang LEFT JOIN satuan_konversi ON barang.id = satuan_konversi.id_produk  AND barang.kode_barang = satuan_konversi.kode_produk WHERE barang.kode_barang = '$kode_barang' OR satuan_konversi.kode_barcode = '$kode_barang' ");
 $jumlah = mysqli_fetch_array($query);
 
 if ($jumlah['jumlah_data'] > 0){

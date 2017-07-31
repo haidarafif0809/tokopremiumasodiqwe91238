566cd8695f8a80da3f4408739fe948c4b3000610
Rikopernando <rikopernando96@gmail.com>
2017-07-31 11:35:40 +0700

    Diskon berdasarkan jumlah barang
    Diskon berdasarkan jumlah barang di edit form penjualan order

--- hapustbs_penjualanorder.php
+++ hapustbs_penjualanorder.php
@@ -16,13 +16,21 @@ $query = $db->query("DELETE FROM tbs_penjualan_order WHERE id = '$id'");
 
 $query2 = $db->query("DELETE FROM tbs_fee_produk WHERE kode_produk = '$kode_barang'");
 
- 
-
-    // UNTUK MENGETAHUI JUMLAAH TBS SEBENARNYA
-    $subtotal_tbs_order = 0;
+     // UNTUK MENGETAHUI JUMLAAH TBS SEBENARNYA
     $jumlah_tbs = 0;
     $potongan_tbs_order = 0;
-    $query_stok_tbs = $db->query("SELECT jumlah_barang,satuan, subtotal, potongan FROM tbs_penjualan_order WHERE kode_barang = '$kode_barang' AND session_id = '$session_id' ");
+
+    if (isset($_POST['no_faktur_order'])) {
+
+    $no_faktur_order = stringdoang($_POST['no_faktur_order']);
+
+    $query_stok_tbs = $db->query("SELECT jumlah_barang,satuan, subtotal, potongan FROM tbs_penjualan_order WHERE kode_barang = '$kode_barang' AND no_faktur_order = '$no_faktur_order' ");
+    }
+    else{
+
+    $query_stok_tbs = $db->query("SELECT jumlah_barang,satuan, potongan FROM tbs_penjualan_order WHERE kode_barang = '$kode_barang' AND session_id = '$session_id' ");	
+    }
+
     while($data_stok_tbs = mysqli_fetch_array($query_stok_tbs)){
 
       $query_cek_satuan_konversi = $db->query("SELECT konversi FROM satuan_konversi WHERE kode_produk = '$kode_barang' AND id_satuan = '$data_stok_tbs[satuan]' ");
@@ -36,11 +44,9 @@ $query2 = $db->query("DELETE FROM tbs_fee_produk WHERE kode_produk = '$kode_bara
 
         $jumlah_tbs = $jumlah_tbs_penjualan + $jumlah_tbs;
         $potongan_tbs_order = $potongan_tbs_order + $data_stok_tbs['potongan'];
-        $subtotal_tbs_order = $subtotal_tbs_order + $data_stok_tbs['subtotal'];
 
     }
   //  UNTUK MENGETAHUI JUMLAAH TBS SEBENARNYA
-
      // ambil setting_diskon_jumlah yang selisih antara jumlah produk dan syarat jumlah lebih dari nol
 	$query = $db->query("SELECT  potongan,  syarat_jumlah FROM setting_diskon_jumlah WHERE kode_barang = '$kode_barang' ");
 	while ($data = mysqli_fetch_array($query)) {// while
@@ -95,15 +101,48 @@ $query2 = $db->query("DELETE FROM tbs_fee_produk WHERE kode_produk = '$kode_bara
                 }
 
 
-                                              # code...
-                          $query1 = $db->prepare("UPDATE tbs_penjualan_order SET subtotal = subtotal - $potongan_tampil , potongan = ? WHERE kode_barang = ? AND session_id = ? LIMIT 1 ");
+			    if (isset($_POST['no_faktur_order'])) {
+			    	
+			    		$no_faktur_order = stringdoang($_POST['no_faktur_order']);
+
+			              $query1 = $db->prepare("UPDATE tbs_penjualan_order SET subtotal = subtotal - $potongan_tampil , potongan = ? WHERE kode_barang = ? AND no_faktur_order = ? LIMIT 1 ");
+
+                          $query1->bind_param("iss",
+                          $potongan_tampil,$kode_barang, $no_faktur_order);
+
+                          $query1->execute();  
+		      	 }else{
+
+		      	         
+		      	         $query1 = $db->prepare("UPDATE tbs_penjualan_order SET subtotal = subtotal - $potongan_tampil , potongan = ? WHERE kode_barang = ? AND session_id = ? LIMIT 1 ");
 
                           $query1->bind_param("iss",
                           $potongan_tampil,$kode_barang, $session_id);
 
-                          $query1->execute();  
+                          $query1->execute();  	
+		      	 }                                       # code...
 
-echo $subtotal_tbs_order;
+
+
+if (isset($_POST['no_faktur_order'])) {
+  
+    $no_faktur = stringdoang($_POST['no_faktur_order']);
+// menampilakn hasil penjumlah subtotal ALIAS total penjualan dari tabel tbs_penjualan berdasarkan data no faktur
+ $query_total = $db->query("SELECT SUM(subtotal) AS total_penjualan FROM tbs_penjualan_order WHERE no_faktur_order = '$no_faktur'");
+
+}else{
+
+
+// menampilakn hasil penjumlah subtotal ALIAS total penjualan dari tabel tbs_penjualan berdasarkan data no faktur
+ $query_total = $db->query("SELECT SUM(subtotal) AS total_penjualan FROM tbs_penjualan_order WHERE session_id = '$session_id'");
+}
+ 
+ // menyimpan data sementara yg ada pada $query
+ $data_total = mysqli_fetch_array($query_total);
+ $total = $data_total['total_penjualan'];
+
+
+echo$total;
 //Untuk Memutuskan Koneksi Ke Database
 mysqli_close($db);   
 ?>

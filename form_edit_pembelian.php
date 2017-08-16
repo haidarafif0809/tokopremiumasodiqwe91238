8c1dce90fa349e3327d1bc5550cf8b59c51d9f16
Hendri Syaiful Nugraha, S.H <hendri.syand@gmail.com>
2017-08-16 15:18:58 +0700

    Penyamaan cek perubahan harga untuk di edit pembeliaan dengan di form sebelumnya, yang beda hanya ada no_fakturnya

--- form_edit_pembelian.php
+++ form_edit_pembelian.php
@@ -1395,10 +1395,13 @@ $("#form_barcode").submit(function(){
       //Cek untuk perubahan harga beli
       $.post("cek_perubahan_harga_edit_pembelian.php",{no_faktur:no_faktur},function(hasil){
         if(hasil == 1){
-          var pesan_alert = confirm("Harga Barang melebihi harga jual, yakin akan merubah harga beli tersebut? ");
+            var pesan_alert = confirm("Harga Barang melebihi harga jual, yakin akan merubah harga beli tersebut? ");
         }
-        else{
-          var pesan_alert = confirm("Ada perubahan pada harga beli, anda yakin ? ");
+        else if(hasil == 2){
+            var pesan_alert = confirm("Ada perubahan pada harga beli, anda yakin ?");
+        }
+        else if(hasil == 3){
+            var pesan_alert = confirm("Harga beli produk tidak ada perubahan, lanjutkan transaksi ?");
         }
 
         if (pesan_alert == true) {

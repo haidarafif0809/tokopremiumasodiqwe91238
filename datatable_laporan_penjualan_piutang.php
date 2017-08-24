c77a6b48077d8c1d0028b42320366886df7377cd
Riko Pernando <rikopernando96@gmail.com>
2017-08-24 15:10:54 +0700

    Perbaiki lap piutang
    Perbaiki lap piutang

--- datatable_laporan_penjualan_piutang.php
+++ datatable_laporan_penjualan_piutang.php
@@ -5,35 +5,16 @@ include 'db.php';
 
 $dari_tanggal = stringdoang($_POST['dari_tanggal']);
 $sampai_tanggal = stringdoang($_POST['sampai_tanggal']);
-
-
-
-$data_sum_dari_detail_pembayaran = 0;
-
-// LOGIKA UNTUK AMBIL BERDASARKAN KONSUMEN DAN SALES (QUERY TAMPIL AWAL)
-  $query_sum_dari_penjualan = $db->query("SELECT no_faktur,SUM(tunai) AS tunai_penjualan,SUM(total) AS total_akhir, SUM(kredit) AS total_kredit FROM penjualan WHERE tanggal >= '$dari_tanggal' AND tanggal <= '$sampai_tanggal' AND kredit != 0 ");
-
-
-
-  $query_faktur_penjualan = $db->query("SELECT no_faktur FROM penjualan WHERE tanggal >= '$dari_tanggal' AND tanggal <= '$sampai_tanggal' AND kredit != 0 ");
-while($data_faktur_penjualan = mysqli_fetch_array($query_faktur_penjualan)){
-
-  $query_sum_dari_detail_pembayaran_piutang = $db->query("SELECT SUM(jumlah_bayar) + SUM(potongan) AS ambil_total_bayar FROM detail_pembayaran_piutang WHERE no_faktur_penjualan = '$data_faktur_penjualan[no_faktur]' ");
-  $data_sum_dari_detail_pembayaran_piutang = mysqli_fetch_array($query_sum_dari_detail_pembayaran_piutang);
-
-  $data_sum_dari_detail_pembayaran = $data_sum_dari_detail_pembayaran + $data_sum_dari_detail_pembayaran_piutang['ambil_total_bayar'];
-// LOGIKA UNTUK  UNTUK AMBIL  BERDASARKAN KONSUMEN DAN SALES (QUERY TAMPIL AWAL)
-}
-
-$data_sum_dari_penjualan = mysqli_fetch_array($query_sum_dari_penjualan);
-$total_akhir = $data_sum_dari_penjualan['total_akhir'];
-$total_kredit = $data_sum_dari_penjualan['total_kredit'];
-$total_bayar = $data_sum_dari_penjualan['tunai_penjualan'] +  $data_sum_dari_detail_pembayaran;
-
+$total_akhir = 0;
+$total_bayar = 0;
+$total_kredit = 0;
+$totalData = 0;
+$totalFiltered = 0;  // when there is no search parameter then total number rows = total number filtered rows.
 
 
 // storing  request (ie, get/post) global array to a variable  
 $requestData= $_REQUEST;
+
 
 $columns = array( 
 // datatable column index  => database column name
@@ -51,19 +32,13 @@ $columns = array(
 // LOGIKA UNTUK FILTER BERDASARKAN KONSUMEN DAN SALES (QUERY TAMPIL AWAL)
 
 // getting total number records without any search
-$sql =" SELECT p.id,p.tanggal,p.tanggal_jt, DATEDIFF(DATE(NOW()), p.tanggal) AS usia_piutang ,p.no_faktur,p.kode_pelanggan,p.total,p.jam,p.status,p.potongan,p.tax,p.sisa,p.kredit, pl.nama_pelanggan,p.nilai_kredit";
+
+$sql =" SELECT p.id,p.tanggal,p.tanggal_jt, DATEDIFF(DATE(NOW()), p.tanggal) AS usia_piutang ,p.no_faktur,p.kode_pelanggan,p.total,p.jam,p.status,p.potongan,p.tax,p.sisa,p.kredit, pl.nama_pelanggan,p.nilai_kredit,p.tunai";
 $sql.=" FROM penjualan p INNER JOIN pelanggan pl ON p.kode_pelanggan = pl.id WHERE p.tanggal >= '$dari_tanggal' AND p.tanggal <= '$sampai_tanggal' AND p.kredit != 0  ";
 
 // LOGIKA UNTUK FILTER BERDASARKAN KONSUMEN DAN SALES (QUERY TAMPIL AWAL)
 
 $query=mysqli_query($conn, $sql) or die("eror 1");
-$totalData = mysqli_num_rows($query);
-$totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
-
-// LOGIKA UNTUK FILTER BERDASARKAN KONSUMEN DAN SALES (QUERY PENCARIAN DATATABLE)
-$sql =" SELECT p.id,p.tanggal,p.tanggal_jt, DATEDIFF(DATE(NOW()), p.tanggal) AS usia_piutang ,p.no_faktur,p.kode_pelanggan,p.total,p.jam,p.status,p.potongan,p.tax,p.sisa,p.kredit, pl.nama_pelanggan,p.nilai_kredit";
-$sql.=" FROM penjualan p INNER JOIN pelanggan pl ON p.kode_pelanggan = pl.id WHERE p.tanggal >= '$dari_tanggal' AND p.tanggal <= '$sampai_tanggal' AND p.kredit != 0  ";
-// LOGIKA UNTUK FILTER BERDASARKAN KONSUMEN DAN SALES (QUERY PENCARIAN DATATABLE)
 
 
 if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
@@ -74,8 +49,25 @@ $sql.=" OR pl.nama_pelanggan LIKE '".$requestData['search']['value']."%' )";
 }
 
 $query=mysqli_query($conn, $sql) or die("eror 2");
-$totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
-$sql.="ORDER BY waktu_input DESC  LIMIT ".$requestData['start']." ,".$requestData['length']." ";
+
+while($data_total = mysqli_fetch_array($query) ) { 
+
+      $query_pembyaran_piutang = $db->query("SELECT SUM(jumlah_bayar) + SUM(potongan) AS total_bayar FROM detail_pembayaran_piutang WHERE no_faktur_penjualan = '$data_total[no_faktur]' ");
+      $data_pembyaran = mysqli_fetch_array($query_pembyaran_piutang);
+
+      $tot_bayar = $data_pembyaran['total_bayar'] + $data_total['tunai'];
+      $sisa_kredit = $data_total['nilai_kredit'] - $data_pembyaran['total_bayar'];
+
+      $total_akhir = $total_akhir + $data_total['total'];
+      $total_bayar = $total_bayar + $tot_bayar;
+      $total_kredit = $total_kredit + $sisa_kredit;
+
+      $totalData = $totalData + 1;
+      $totalFiltered = $totalData; 
+
+}
+
+$sql.="ORDER BY waktu_input ".$requestData['order'][0]['dir']."  LIMIT ".$requestData['start']." ,".$requestData['length']." ";
 /* $requestData['order'][0]['column'] contains colmun index, $requestData['order'][0]['dir'] contains order such as asc/desc  */  
 $query=mysqli_query($conn, $sql) or die("eror 3");
 
@@ -84,17 +76,11 @@ while( $row=mysqli_fetch_array($query) ) {  // preparing an array
   $nestedData=array(); 
 
 
-$query0232 = $db->query("SELECT SUM(jumlah_bayar) + SUM(potongan) AS total_bayar FROM detail_pembayaran_piutang WHERE no_faktur_penjualan = '$row[no_faktur]' ");
-$kel_bayar = mysqli_fetch_array($query0232);
+      $query0232 = $db->query("SELECT SUM(jumlah_bayar) + SUM(potongan) AS total_bayar FROM detail_pembayaran_piutang WHERE no_faktur_penjualan = '$row[no_faktur]' ");
+      $kel_bayar = mysqli_fetch_array($query0232);
 
-$sum_dp = $db->query("SELECT SUM(tunai) AS tunai_penjualan,nilai_kredit FROM penjualan WHERE no_faktur = '$row[no_faktur]' ");
-$data_sum = mysqli_fetch_array($sum_dp);
-$Dp = $data_sum['tunai_penjualan'];
-
-$num_rows = mysqli_num_rows($query0232);
-
-$tot_bayar = $kel_bayar['total_bayar'] + $Dp;
-$sisa_kredit = $row['nilai_kredit'] - $tot_bayar;
+      $tot_bayar = $kel_bayar['total_bayar'] + $row['tunai'];
+      $sisa_kredit = $row['nilai_kredit'] - $kel_bayar['total_bayar'];
 
       $nestedData[] = $row['no_faktur'];
       $nestedData[] = $row['nama_pelanggan'];
@@ -103,15 +89,8 @@ $sisa_kredit = $row['nilai_kredit'] - $tot_bayar;
       $nestedData[] =  "<p align='right'>".rp($row['usia_piutang'])." Hari</p>";
       $nestedData[] =  "<p align='right'>".rp($row['total'])."</p>";
 
-      if ($num_rows > 0 )
-      {
-      	$nestedData[] =  "<p align='right'>".rp($tot_bayar)."</p>";
-      }
-      else
-      {
-      	$nestedData[] = "<p>0</p>";
+      $nestedData[] =  "<p align='right'>".rp($tot_bayar)."</p>";
 
-      }
 
       if ($sisa_kredit < 0 ) {
         # code...
@@ -122,6 +101,8 @@ $sisa_kredit = $row['nilai_kredit'] - $tot_bayar;
       }
 
   $data[] = $nestedData;
+
+
 }
 
 
@@ -145,7 +126,7 @@ $nestedData=array();
 $json_data = array(
       "draw"            => intval( $requestData['draw'] ),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
       "recordsTotal"    => intval( $totalData ),  // total number of records
-      "recordsFiltered" => intval( $totalData ), // total number of records after searching, if there is no searching then totalFiltered = totalData
+      "recordsFiltered" => intval( $totalFiltered ), // total number of records after searching, if there is no searching then totalFiltered = totalData
       "data"            => $data   // total data array
       );
 
